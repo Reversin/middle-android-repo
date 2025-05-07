245de75ece68472bf1ab131392036b2b77de8342
Reversin <reversin@inbox.ru>
2025-05-07 20:16:36 +0300

    completed

--- app/src/main/java/com/example/androidpracticumcustomview/ComposeScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ComposeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.unit.sp
 
 class ComposeScreen : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
